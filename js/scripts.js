52a49000ff68e98c299a9274cb02486ac3f8cd4e
maxphog <maxphog@gmail.com>
2024-03-02 15:10:52 -0500

    testing audio file switching, and some formatting here andthere

--- js/scripts.js
+++ js/scripts.js
@@ -21,93 +21,30 @@ dom_MAP.setAttribute("controls","");
 
 // append AP as the new first child of the wrapper
 dom_APW.insertBefore(dom_MAP, dom_APW.firstChild); 
+
+// dom_MAP.setAttribute("src", "/browser_media_player/test_sounds/t-rex-roar.mp3")
+
 console.log(dom_MAP);
 
-
-
-/** OLDCODE VVVV */
-// // initialization of metadata value objects
-// let duration;
-// let author;
-// let title;
-
-// // initialization of metadata updating function
-// function updateMetadataInfo(metadata) {
-//     let dom_MI = document.getElementById("metadataInfo");
-//     dom_MI.innerHTML = metadata;
-// }
-
-// // once document fully loads 
-// document.addEventListener("DOMContentLoaded", function() {
-//     console.log("DOM fully loaded and parsed");
-    
-//     console.log("HELOOOOOOOO WORLLLLDDDDD!!!");
-
-    
-//     console.log("Our Html elements:");
-//     // document body (body)
-//     let dom_body = document.getElementsByTagName("body")
-//     console.log(dom_body);
-
-    
-//     // document Audio Player Wrapper (APW)
-//     let dom_APW = document.getElementById("audioPlayerWrapper")
-//     console.log(dom_APW);
-
-    
-//     // document Audio Player (AP)
-//     let dom_AP = document.getElementById("audioPlayer")
-//     console.log(dom_AP);
-
-    
-//     // document Metadata Info (MI)
-//     let dom_MI = document.getElementById("metadataInfo")
-//     console.log(dom_MI);
-
-    
-//     // document Toggle Loop Button (TLBTN)
-//     let dom_TLBTN = document.getElementById("toggleLoopBtn")
-//     console.log(dom_TLBTN);
-
-    
-//     // document Download Link (DL)
-//     let dom_DL = document.getElementById("downloadLink")
-//     console.log(dom_DL);
-
-//     // adding an event listener for when the audio file finishes loading.
-//     dom_AP.addEventListener("loadedmetadata", function() {
-//         // the moment we get a loadedmetadata event from the audio player
-//         console.log("Metadata loaded!");
-//         console.log(dom_AP);
-
-//         // getting the duration of the audio file from the audio element
-//         let duration = dom_AP.duration;
-//         // lets try author, title, and other metadata
-//         let  author = dom_AP.author ;
-//         let  title = dom_AP.title;
-        
-//         console.log("The duration is:", duration);
-//         console.log("The author is:", author);
-//         console.log("The title is:", title);
-        
-//         // updating metadata 
-//         updateMetadataInfo(`
-//             Duration: ${duration} 
-//             <br>
-//             Author: ${author}
-//             <br>
-//             Title: ${title}
-//         `);
-//     });
-
-//     // updating metadata 
-//     updateMetadataInfo(`
-//         Duration: ${duration} 
-//         <br>
-//         Author: ${author}
-//         <br>
-//         Title: ${title}
-//     `);
-// });
+/**
+ *  This function sets the src attribute of our audio elements. 
+ * @param {Object} audioElement - take the audio element we want to set the source for  
+ * @param {String} source  - the URI source the audio element will grab the audio from
+ * 
+ * @returns null
+ */
+function setAudioSource(audioElement, source) {
+    audioElement.setAttribute("src", source)
+    console.log("Audio source set:", audioElement);
+    return null
+}
+// testing audio file switching
+// setAudioSource(dom_MAP,"/browser_media_player/test_sounds/t-rex-roar.mp3");
+// setTimeout(function() {
+//     setAudioSource(dom_MAP, "/browser_media_player/test_sounds/THUN_Thunder claps with long ring out; variation_CS_USC.mp3");
+// }, 1000);
+// setTimeout(function() {
+//     setAudioSource(dom_MAP,"/browser_media_player/test_sounds/AMBTrop_Jungle background 1_CS_USC.mp3");
+// }, 2000);
 
 
